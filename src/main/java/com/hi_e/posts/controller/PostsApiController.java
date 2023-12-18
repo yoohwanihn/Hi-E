4c01fe05ae973ac764c3fbe7220a6044377591a0
치타유환인 <73772238+yoohwanihn@users.noreply.github.com>
2023-12-18 14:28:09 +0900

    경로수정2

--- src/main/java/com/hi_e/posts/controller/PostsApiController.java
+++ src/main/java/com/hi_e/posts/controller/PostsApiController.java
@@ -1,8 +1,5 @@
 package com.hi_e.posts.controller;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
