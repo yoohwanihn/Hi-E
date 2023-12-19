fcda0f74ea393d1b7be96a0d838dbbf4565fe2fe
치타유환인 <73772238+yoohwanihn@users.noreply.github.com>
2023-12-19 17:15:16 +0900

    에러페이지 추가

---
+++ src/main/java/error/Error.java
@@ -0,0 +1,31 @@
+package error;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class Error implements ErrorController {
+    private String ERROR_TEMPLATES_PATH = "/errors/";
+
+    @RequestMapping(value = "/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if(status != null){
+        	
+            int statusCode = Integer.valueOf(status.toString());
+            if(statusCode == HttpStatus.NOT_FOUND.value()){
+                return ERROR_TEMPLATES_PATH + "404";
+            }
+
+            if(statusCode == HttpStatus.FORBIDDEN.value()){
+                return ERROR_TEMPLATES_PATH + "500";
+            }
+        }
+        return "error";
+    }
+}
