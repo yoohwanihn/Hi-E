4f1e2ff891f6ffff2e44fad9d051b99c2726a4b6
minji856 <sistar96@gmail.com>
2024-01-09 01:11:42 +0900

    cors 관련 설정 추가

--- src/main/java/com/hi_e/springsecurity/config/WebMvcConfig.java
+++ src/main/java/com/hi_e/springsecurity/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.hi_e.springsecurity.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,5 +22,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 						"classpath:/static/", "classpath:/templates/") // 반드시 / 로 끝나야함
 				.setCachePeriod(20); // 캐시 지속시간 설정 (초)
     }
-
+    
+    /* Cors 관련 설정 메서드입니다. */
+    @Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedOrigins("http://localhost:3000")
+		.allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+	}
 }
